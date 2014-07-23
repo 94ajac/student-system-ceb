091708aef218f384f691c2f6c2a9717548957bc5
georocha24@gmail.com <georocha24@gmail.com@687ce17d-ffc1-4334-ec52-23b5d808f9a3>
2014-07-23 04:30:07 +0000

    modificacion de Form1

--- trunk/Form1.Designer.vb
+++ trunk/Form1.Designer.vb
@@ -23,27 +23,41 @@ Partial Class Form1
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
         Me.CheckButton1 = New DevExpress.XtraEditors.CheckButton()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.GroupBox1.SuspendLayout()
         Me.SuspendLayout()
         '
         'CheckButton1
         '
-        Me.CheckButton1.Location = New System.Drawing.Point(163, 129)
+        Me.CheckButton1.Location = New System.Drawing.Point(24, 37)
         Me.CheckButton1.Name = "CheckButton1"
         Me.CheckButton1.Size = New System.Drawing.Size(75, 23)
         Me.CheckButton1.TabIndex = 0
         Me.CheckButton1.Text = "CheckButton1"
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.CheckButton1)
+        Me.GroupBox1.Location = New System.Drawing.Point(23, 12)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(200, 100)
+        Me.GroupBox1.TabIndex = 1
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "GroupBox1"
         '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(284, 261)
-        Me.Controls.Add(Me.CheckButton1)
+        Me.Controls.Add(Me.GroupBox1)
         Me.Name = "Form1"
         Me.Text = "ti"
+        Me.GroupBox1.ResumeLayout(False)
         Me.ResumeLayout(False)
 
     End Sub
     Friend WithEvents CheckButton1 As DevExpress.XtraEditors.CheckButton
+    Friend WithEvents GroupBox1 As System.Windows.Forms.GroupBox
 
 End Class
